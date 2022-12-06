521130ff9668ea11d8f99397964bd6a63642b7e2
forsilence <3312034934@qq.com>
2022-12-06 15:19:21 +0800

    add range calculate test

--- main_test.ts
+++ main_test.ts
@@ -5,4 +5,5 @@ Deno.test("test number",() =>
 {
   assertEquals( rc_eval("1 + 2").number, 3 )
   assertEquals( rc_eval("1 + 2 + 4").number, 7 )
+  console.log(rc_eval("(1 + 2) + (4 ~ 5) - (3 + 4 )"));
 });
