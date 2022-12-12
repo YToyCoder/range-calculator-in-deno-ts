a4653a3d9a51b1210c4d48713b0993d09fc8197e
forsilence <3312034934@qq.com>
2022-12-12 16:32:49 +0800

    error print red

--- eval.ts
+++ eval.ts
@@ -1,10 +1,11 @@
 import { rc_eval } from "./main.ts"
+import * as Colors from "https://deno.land/std/fmt/colors.ts"
 
 Deno.args.forEach(value => {
   try {
     const r = rc_eval(value)
     console.log(r.toString())
   }catch(e){
-    console.error(e.toString());
+    console.error(Colors.red(e.toString()));
   }
 })
