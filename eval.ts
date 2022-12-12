427396795b8744d5c01c84dc817b236724498104
forsilence <3312034934@qq.com>
2022-12-12 17:01:31 +0800

    colorful log

--- eval.ts
+++ eval.ts
@@ -4,7 +4,7 @@ import * as Colors from "https://deno.land/std/fmt/colors.ts"
 Deno.args.forEach(value => {
   try {
     const r = rc_eval(value)
-    console.log(r.toString())
+    console.log(Colors.green(r.toString()))
   }catch(e){
     console.error(Colors.red(e.toString()));
   }
